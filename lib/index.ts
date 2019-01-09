030685ab02c858d93a6fc6a093d4ff687252b5ea
rpgeeganage <rpgeeg@gmail.com>
2019-01-09 23:12:59 +0100

    added tests

--- lib/index.ts
+++ lib/index.ts
@@ -49,7 +49,7 @@ class Alls {
    */
   process(): Promise<ReturnValue[]> {
     const promisesList = this.promises.map((p) => {
-      if (!this.isPromise(p)) {
+      if (!p || p.toString() !== '[object Promise]') {
         return {
           state: 'fulfilled',
           value: p
@@ -76,25 +76,6 @@ class Alls {
     });
 
     return Promise.all(promisesList as Promise<any>[]);
-  }
-
-  /**
-   * Check given object is a promise
-   *
-   * @returns {boolean}
-   * @memberof Alls
-   */
-  private isPromise(p: any) {
-    if (!p) {
-      return false;
-    }
-
-    if (p.toString() === '[object Promise]') {
-      return true;
-    }
-
-    // For none native promises
-    return p && typeof p['then'] === 'function';
   }
 }
 
